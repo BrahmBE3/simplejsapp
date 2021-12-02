8c1e56304e616a064161d8d7d377c38e05c5fba2
BrahmBE3 <91154950+BrahmBE3@users.noreply.github.com>
2021-12-02 21:23:45 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+alert('hello world');
+let favroriteFood = 'pizza';
+document.write(favroriteFood);
+favroriteFood = 'burger';
+document.write(favroriteFood);
+favroriteFood = 'almonds';
+document.write(favroriteFood);
