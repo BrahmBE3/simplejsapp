6366f816ef829818380d4dd0077524726df22232
BrahmBE3 <brahmdeepbains123@gmail.com>
2021-12-04 19:09:30 +0100

    update for the Pokedex
    Title name changed. Pokémon list made with 7 Pokémon names, types and category

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-alert('hello world');
-let favroriteFood = 'pizza';
-document.write(favroriteFood);
-favroriteFood = 'burger';
-document.write(favroriteFood);
-favroriteFood = 'almonds';
-document.write(favroriteFood);
+let pokemonList=[
+  {name:'Dragonite' , type:['dragon','flying'] , Category:'dragon' },
+  {name:'Charmeleon' , type:['Fire'] , Category:'Flame'},
+  {name:'Charizard' , type:['Fire','Flying'] , Category:'Flame'},
+  {name:'Blastoise' , type:['Water'] , Category:'Shellfish'},
+  {name:'Growlithe' , type:['Fire'] , Category:'Puppy'},
+  {name:'Raichu' ,  type:['Electric'] , Category:'Mouse'},
+  {name:'Machoke' ,  type:['Fighting'] , Category:'Superpower'},
+  {name:'Jigglypuff' , type:['Normal','Fairy'] , Category:'Balloon'}
+];
