6a455f9e9a993ac0c1d185ff6042690fe27d339a
BrahmBE3 <brahmdeepbains123@gmail.com>
2021-12-05 14:02:28 +0100

    Update script.js
    height  property added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 let pokemonList=[
-  {name:'Dragonite' , types:['dragon','flying'] , Category:'dragon' },
-  {name:'Charmeleon' , types:['Fire'] , Category:'Flame'},
-  {name:'Charizard' , types:['Fire','Flying'] , Category:'Flame'},
-  {name:'Blastoise' , types:['Water'] , Category:'Shellfish'},
-  {name:'Growlithe' , types:['Fire'] , Category:'Puppy'},
-  {name:'Raichu' ,  types:['Electric'] , Category:'Mouse'},
-  {name:'Machoke' ,  types:['Fighting'] , Category:'Superpower'},
-  {name:'Jigglypuff' , types:['Normal','Fairy'] , Category:'Balloon'}
+  {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon' },
+  {name:'Charmeleon' ,  height: 5.5 , types:['Fire'] , Category:'Flame'},
+  {name:'Charizard' ,  height: 5.9 , types:['Fire','Flying'] , Category:'Flame'},
+  {name:'Blastoise' ,  height: 4.0  , types:['Water'] , Category:'Shellfish'},
+  {name:'Growlithe' ,  height: 3.5 , types:['Fire'] , Category:'Puppy'},
+  {name:'Raichu' ,   height: 5.0 , types:['Electric'] , Category:'Mouse'},
+  {name:'Machoke' ,   height: 4.2 , types:['Fighting'] , Category:'Superpower'},
+  {name:'Jigglypuff' ,  height: 2.2 , types:['Normal','Fairy'] , Category:'Balloon'}
 ];
