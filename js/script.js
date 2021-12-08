d7dd7138918ea3f9109de7d664f423cf838200ac
BrahmBE3 <brahmdeepbains123@gmail.com>
2021-12-08 22:07:14 +0100

    update
    Pokémon list with object defining Name and height property based on the parameter of height.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-let pokemonList=[
-  {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon' },
+let pokemonList = [
+  {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon'},
   {name:'Charmeleon' ,  height: 5.5 , types:['Fire'] , Category:'Flame'},
   {name:'Charizard' ,  height: 5.9 , types:['Fire','Flying'] , Category:'Flame'},
-  {name:'Blastoise' ,  height: 4.0  , types:['Water'] , Category:'Shellfish'},
-  {name:'Growlithe' ,  height: 3.5 , types:['Fire'] , Category:'Puppy'},
+  {name:'Blastoise' ,  height: 2.0  , types:['Water'] , Category:'Shellfish'},
+  {name:'Growlithe' ,  height: 2.5 , types:['Fire'] , Category:'Puppy'},
   {name:'Raichu' ,   height: 5.0 , types:['Electric'] , Category:'Mouse'},
-  {name:'Machoke' ,   height: 4.2 , types:['Fighting'] , Category:'Superpower'},
+  {name:'Machoke' ,   height: 2.2 , types:['Fighting'] , Category:'Superpower'},
   {name:'Jigglypuff' ,  height: 2.2 , types:['Normal','Fairy'] , Category:'Balloon'}
 ];
+
+for ( var i = 0 ; i < pokemonList.length ; i++){
+  /* For got three steeps in it (1. starter ||  initialzer gives you information about vere loop have to start ike a starting point) ; (2. condetion where should loop needs to end to which number or point);(3.Incremantor or decremaantor goes both direction ascending and decending represent as (i++ or i+1)(i-- or i-1))*/
+if (pokemonList[i].height < 8.0 && pokemonList[i].height > 5.0 ) {
+  document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '  wow! thats big pokemon  ');
+}
+else if (pokemonList[i].height < 5.0 && pokemonList[i].height > 3.0) {
+  document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '  wow! thats average size pokemon  ');
+}
+else {
+document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '  small pokemon  ');
+}
+}
