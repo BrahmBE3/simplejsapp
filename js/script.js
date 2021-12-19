11b0ccae7d68d02e9403f9a2c425e6db16d67b9c
BrahmBE3 <brahmdeepbains123@gmail.com>
2021-12-19 23:56:08 +0100

    Update script.js
    for each and fuction and returns

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//IIfe fuction , global variable
+
+let pokemoRepository = (function() {
 let pokemonList = [
   {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon'},
   {name:'Charmeleon' ,  height: 5.5 , types:['Fire'] , Category:'Flame'},
@@ -8,16 +11,37 @@ let pokemonList = [
   {name:'Machoke' ,   height: 2.2 , types:['Fighting'] , Category:'Superpower'},
   {name:'Jigglypuff' ,  height: 2.2 , types:['Normal','Fairy'] , Category:'Balloon'}
 ];
+function getAll() {
+  return pokemonList;
+}
+function add(pokemon){
+   pokemonlist.push(pokemon);
+}
+return{
+  getAll:  getAll,
+  add: add
+};
+})();
+ pokemonList.forEach(function(pokemon)  {
+   if (pokemon.height =< 5.0) {
+  document.write('Name:' + pokemon.name + '<br>' + 'Height:' + pokemon.height + ' thats a small pokemon');
+} else {
+    document.write('Name:' + pokemon.name +'<br>' + 'Height:' + pokemon.height + ' Wow thats a big pokemon');
+  }
+}
+);
+console.log(pokemonRepository.getAll());
 
-for ( var i = 0 ; i < pokemonList.length ; i++){
-  /* For got three steeps in it (1. starter ||  initialzer gives you information about vere loop have to start ike a starting point) ; (2. condetion where should loop needs to end to which number or point);(3.Incremantor or decremaantor goes both direction ascending and decending represent as (i++ or i+1)(i-- or i-1))*/
-if (pokemonList[i].height < 8.0 && pokemonList[i].height > 5.0 ) {
-  document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats big pokemon  ');
-}
-else if (pokemonList[i].height < 5.0 && pokemonList[i].height > 3.0) {
-  document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats average size pokemon  ');
-}
-else {
-document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  small pokemon  ');
-}
-}
+
+// for ( var i = 0 ; i < pokemonList.length ; i++){
+//   /* For got three steeps in it (1. starter ||  initialzer gives you information about vere loop have to start ike a starting point) ; (2. condetion where should loop needs to end to which number or point);(3.Incremantor or decremaantor goes both direction ascending and decending represent as (i++ or i+1)(i-- or i-1))*/
+// if (pokemonList[i].height < 8.0 && pokemonList[i].height > 5.0 ) {
+//   document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats big pokemon  ');
+// }
+// else if (pokemonList[i].height < 5.0 && pokemonList[i].height > 3.0) {
+//   document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats average size pokemon  ');
+// }
+// else {
+// document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  small pokemon  ');
+// }
+// }
