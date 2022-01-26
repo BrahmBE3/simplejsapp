4b04d130f6d774e90116f67e6cff5a97b658259d
BrahmBE3 <brahmdeepbains123@gmail.com>
2022-01-26 20:53:48 +0100

    update of DOM
    Deletion of old comment and adding new to it .

--- js/script.js
+++ js/script.js
@@ -1,67 +1,53 @@
-//IIfe fuction , global variable
-
 let pokemonRepository = (function() {
-let pokemonList = [
-  {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon'},
-  {name:'Charmeleon' ,  height: 5.5 , types:['Fire'] , Category:'Flame'},
-  {name:'Charizard' ,  height: 5.9 , types:['Fire','Flying'] , Category:'Flame'},
-  {name:'Blastoise' ,  height: 2.0  , types:['Water'] , Category:'Shellfish'},
-  {name:'Growlithe' ,  height: 2.5 , types:['Fire'] , Category:'Puppy'},
-  {name:'Raichu' ,   height: 5.0 , types:['Electric'] , Category:'Mouse'},
-  {name:'Machoke' ,   height: 2.2 , types:['Fighting'] , Category:'Superpower'},
-  {name:'Jigglypuff' ,  height: 2.8 , types:['Normal','Fairy'] , Category:'Balloon'}
-];
-function getAll() {
-  return pokemonList;
-}
-function add(pokemon){
-   pokemonList.push(pokemon);
-}
+    let pokemonList = [
+      {name:'Dragonite' , height: 7.1 , types:['dragon','flying'] , Category:'dragon'},
+      {name:'Charmeleon' ,  height: 5.5 , types:['Fire'] , Category:'Flame'},
+      {name:'Charizard' ,  height: 5.9 , types:['Fire','Flying'] , Category:'Flame'},
+      {name:'Blastoise' ,  height: 2.0  , types:['Water'] , Category:'Shellfish'},
+      {name:'Growlithe' ,  height: 2.5 , types:['Fire'] , Category:'Puppy'},
+      {name:'Raichu' ,   height: 5.0 , types:['Electric'] , Category:'Mouse'},
+      {name:'Machoke' ,   height: 2.2 , types:['Fighting'] , Category:'Superpower'},
+      {name:'Jigglypuff' ,  height: 2.8 , types:['Normal','Fairy'] , Category:'Balloon'}
+    ];
 
-function addListItem (pokemon) {
-let ulElement = document.querySelector(".pokemon-list");
-let listElement = document.createElement("li");
-let button = document.createElement("Button");
-button.innerText = pokemon.name;
-button.classList.add("add-button");
-listElement.appendChild(button);
-ulElement.appendChild(listElement);
+    function add(pokemon) {
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "types" in pokemon
+      ) {
+       pokemonList.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+      }
+    }
+    function getAll() {
+      return pokemonList;
+    }
+    function addListItem(pokemon){
+      let ulElement = document.querySelector(".pokemon-list");
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("add-button");
+      listpokemon.appendChild(button);
+      ulElement.appendChild(listpokemon);
+      button.addEventListener("click", (event) => {
+     showDetails(pokemon);
+   });
+};
+    return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem
+    };
+  })();
+// adding the new pokemon to pokemonlist repositorey
+  pokemonRepository.add({ name: "bulbasauras", height: 1.3, types: ["water"], Category:"dragon" });
 
-button.addEventListener("click", function () {
-   showDetails(pokemon);
-   console.log(pokemon);
+  console.log(pokemonRepository.getAll());
 
-
- return{
-   getAll:  getAll,
-   add: add,
-   addListItem: addListItem
- };
- }
-   // console.log(pokemon);
-//    if (pokemon.height <= 4.0) {
-//    document.write('<p>'+'Name:' +  pokemon.name +  '<br>' + 'Height:' + pokemon.height +'   ' + 'Thats a small pokemon' +'</p>');
-// } else {
-//     document.write('<p>'+'Name:'  +  pokemon.name +'<br>' + 'Height:' + pokemon.height +'    ' + 'WOW! thats a big pokemon' +'</p>');
-//   }
-});
-pokemonRepository.getAll().forEach(function(pokemon) =>  {
-pokemonRepository.addListItem(pokemon);
-
-});
-
-
-
-
-// for ( var i = 0 ; i < pokemonList.length ; i++){
-//   /* For got three steeps in it (1. starter ||  initialzer gives you information about vere loop have to start ike a starting point) ; (2. condetion where should loop needs to end to which number or point);(3.Incremantor or decremaantor goes both direction ascending and decending represent as (i++ or i+1)(i-- or i-1))*/
-// if (pokemonList[i].height < 8.0 && pokemonList[i].height > 5.0 ) {
-//   document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats big pokemon  ');
-// }
-// else if (pokemonList[i].height < 5.0 && pokemonList[i].height > 3.0) {
-//   document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  wow! thats average size pokemon  ');
-// }
-// else {
-// document.write(pokemonList[i].name + '  ' + pokemonList[i].height + ' ' + '-  small pokemon  ');
-// }
-// }
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
