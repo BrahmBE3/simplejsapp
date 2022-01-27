d99d80f0c15b01011521c2d0f90bee55aff8b42d
BrahmBE3 <brahmdeepbains123@gmail.com>
2022-01-27 16:49:05 +0100

    complex ui

--- js/script.js
+++ js/script.js
@@ -126,6 +126,16 @@ function loadDetails(item) {
 
 })();
 
+let container = document.querySelector('#image-container');
+
+// Create an <img> element
+let myImage = document.createElement('img');
+
+// setting `src` property to set the actual element's `src` attribute
+// this also works on <img> elements selected by querySelector() method, it is not specific for <img> elements created with createElement() methods
+myImage.src = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+container.appendChild(myImage);
     return {
       add: add,
       getAll: getAll,
