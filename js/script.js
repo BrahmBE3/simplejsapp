a7c14bbe258db83e8f99094199c416b57249d410
BrahmBE3 <brahmdeepbains123@gmail.com>
2022-02-21 12:52:36 +0100

    Updates For JAVASCRIPT
    Java script updates with working modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
   //IIFE FUCTION ADDED
 let pokemonRepository = (function () {
+<<<<<<< Updated upstream
 
   let pokemonList =
   [];
@@ -67,6 +68,71 @@ function showDetails(pokemon) {
     showModal(pokemon);
  });
 }
+=======
+
+  let pokemonList =
+  [//Give pokemon in the first few taskes
+  // {
+  //   name: "Bulbasaur",
+  //   height: 0.5,
+
+  //   types: ["grass", "poison"],
+  // },
+  // {
+  //   name: "Lucario",
+  //   height: 1.5,
+  //   types: ["fire", "ground"],
+  // },
+  // {
+  //   name: "Squirtle",
+  //   height: 1,
+  //   types: ["water"],
+  // },
+  // {
+  //   name: "Eevee",
+  //   height: 1.1,
+  //   types: ["water"],
+  // },];
+  // fetching pokemon data with API,(150 pokemons)
+];
+
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+
+  let modalContainer = document.querySelector("#modal-container");
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
+      // "height" in pokemon &&
+      // "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else
+    {
+    console.log("pokemon is not correct");
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener("click" ,function (event) {
+      showDetails(pokemon);
+    })
+  }
+
+>>>>>>> Stashed changes
     // adding the loadlist() function
   function loadList() {
     return fetch(apiUrl).then(function (response) {
@@ -105,6 +171,7 @@ function showDetails(pokemon) {
   }
     // creating show modal
   function showModal(pokemon) {
+<<<<<<< Updated upstream
     let modalBody = $(".modal-body");
     let modalTitle = $(".modal-title");
     // let modalHeader = $(".modal-header");
@@ -187,6 +254,64 @@ function showDetails(pokemon) {
   //       hideModal();
   //     }
   //   });
+=======
+    // let modalConatiner = document.querySelector("#modal-container")
+    modalContainer.innerText = "";
+    //craeting div element
+    let modal = document.createElement("div");
+    // adding class to the div elemnt created above
+    modal.classList.add("modal");
+    //close button when clicked close off
+    let closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
+    // creating content for modal
+    let modalTitle = document.createElement("h1");
+    modalTitle.innerText = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+    let modalText = document.createElement("p");
+    modalText.innerText = pokemon.height;
+
+    let modalImg = document.createElement("img");
+    modalImg.classList.add("modal-img");
+    modalImg.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(modalTitle);
+    modal.appendChild(modalText);
+    modal.appendChild(modalImg);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add("is-visible");
+  }
+
+  function hideModal(){
+    modalContainer.classList.remove("is-visible");
+  }
+
+  modalContainer.addEventListener("click", (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+
+  window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+        hideModal();
+      }
+    });
+
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      showModal(pokemon);
+   });
+  }
+>>>>>>> Stashed changes
 
   return {
     add: add,
@@ -196,7 +321,11 @@ function showDetails(pokemon) {
     loadDetails: loadDetails,
     showDetails: showDetails,
     showModal: showModal,
+<<<<<<< Updated upstream
     // hideModal: hideModal
+=======
+    hideModal: hideModal
+>>>>>>> Stashed changes
   };
 })();
 //adding a pokemon into the pokemonRepository
