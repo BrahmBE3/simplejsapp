6075068819f4dee62b826f4f9fa1c2b89b08e921
BrahmBE3 <brahmdeepbains123@gmail.com>
2022-02-24 05:51:54 +0100

    update
    ES linter ,minified CSS and JS added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # simplejsapp
+JavaScript  # simplejsapp
  Pokedox-app  using JavaScript, HTML and CSS with jQuery and bootstrap, working modal giving details about the Pokémon with image.
 Search bar give a no time result for Pokémon to be looked upon.
--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
-
 function addListItem (pokemon){
   let newPokemonList = document.querySelector(".pokemon-list");
   let pokemonListItems = document.createElement("li");
@@ -54,45 +53,6 @@ function showDetails(pokemon) {
     showModal(pokemon);
  });
 }
-
-
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
-
-  let modalContainer = document.querySelector("#modal-container");
-
-  function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "detailsUrl" in pokemon
-      // "height" in pokemon &&
-      // "types" in pokemon
-    ) {
-      pokemonList.push(pokemon);
-    } else
-    {
-    console.log("pokemon is not correct");
-    }
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = pokemon.name;
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
-    button.addEventListener("click" ,function (event) {
-      showDetails(pokemon);
-    })
-  }
-
-
     // adding the loadlist() function
   function loadList() {
     return fetch(apiUrl).then(function (response) {
@@ -164,63 +124,7 @@ function showDetails(pokemon) {
           });
       });
 
-    // let modalConatiner = document.querySelector("#modal-container")
-    modalContainer.innerText = "";
-    //craeting div element
-    let modal = document.createElement("div");
-    // adding class to the div elemnt created above
-    modal.classList.add("modal");
-    //close button when clicked close off
-    let closeButtonElement = document.createElement("button");
-    closeButtonElement.classList.add("modal-close");
-    closeButtonElement.innerText = 'X';
-    closeButtonElement.addEventListener('click', hideModal);
-    // creating content for modal
-    let modalTitle = document.createElement("h1");
-    modalTitle.innerText = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
-    let modalText = document.createElement("p");
-    modalText.innerText = pokemon.height;
 
-    let modalImg = document.createElement("img");
-    modalImg.classList.add("modal-img");
-    modalImg.src = pokemon.imageUrl;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(modalTitle);
-    modal.appendChild(modalText);
-    modal.appendChild(modalImg);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add("is-visible");
-  }
-
-  function hideModal(){
-    modalContainer.classList.remove("is-visible");
-  }
-
-  modalContainer.addEventListener("click", (e) => {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
-    }
-  });
-
-
-  window.addEventListener("keydown", (e) => {
-      if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
-        hideModal();
-      }
-    });
-
-
-  function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      showModal(pokemon);
-   });
-  }
->>>>>>> Stashed changes
 
   return {
     add: add,
@@ -229,13 +133,11 @@ function showDetails(pokemon) {
     loadList: loadList,
     loadDetails: loadDetails,
     showDetails: showDetails,
-    showModal: showModal
+    showModal: showModal,
+    // hideModal: hideModal
   };
 })();
-//adding a pokemon into the pokemonRepository
-// pokemonRepository.add({ name: "Balbasur", height: 0.4, types: ["water"] });
-//
-// console.log(pokemonRepository.getAll());
+
 pokemonRepository.loadList().then(function(){
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
